92f9c4dbed4e19169e6ac98cf38f1edb40ab1fd4
eatarandom <dan@eatarandom.com>
2013-03-25 11:36:01 -0400

    added test folder, tests, and test dependencies

---
+++ test/test.js
@@ -0,0 +1,12 @@
+
+var chai = require('chai'),
+    expect = chai.expect,
+    should = chai.should();
+
+describe('testing init',function(){
+  
+    it('test is test',function(){
+        expect('bar').to.be.a('string');
+    });
+
+});
